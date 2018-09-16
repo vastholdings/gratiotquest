5dbecaca3ab85e97c59d77cf21e3b87ac8d9a759
Colin <colin.diesh@gmail.com>
2018-09-16 15:17:11 -0400

    Use new eslintrc

--- index.js
+++ index.js
@@ -1,113 +1,119 @@
 // Dependencies
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var socketIO = require('socket.io');
-var app = express();
-var server = http.Server(app);
-var io = socketIO(server);
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const socketIO = require('socket.io')
 
-var pg = require('pg');
-const url = require('url');
+const app = express()
+const server = http.Server(app)
+const io = socketIO(server)
 
-const params = url.parse(process.env.DATABASE_URL || 'postgres://user:pass@localhost:5432/gratiotquest');
-const auth = params.auth.split(':');
+const pg = require('pg')
+const url = require('url')
+
+const params = url.parse(
+  process.env.DATABASE_URL ||
+    'postgres://user:pass@localhost:5432/gratiotquest',
+)
+const auth = params.auth.split(':')
 
 const config = {
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1],
-    ssl: true
-};
-var pool = new pg.Pool(config);
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  ssl: true,
+}
+const pool = new pg.Pool(config)
 
+app.set('port', 5000)
+app.use('/static', express.static(`${__dirname}/static`))
 
-app.set('port', 5000);
-app.use('/static', express.static(__dirname + '/static'));
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname, 'static/index.html'))
+})
 
+server.listen(5000)
+console.log('Started on port 5000')
 
+const players = {}
+const catfood = {}
+let items = 0
 
+// Add the WebSocket handlers
+io.on('connection', socket => {
+  console.log('a user connected', socket.id)
+  socket.on('new player', data => {
+    const x = Math.floor(Math.random() * 1000) + 2000
+    const y = Math.floor(Math.random() * 800) + 1000
+    players[socket.id] = {
+      x,
+      y,
+      username: data,
+    }
+    socket.emit('initstate', socket.id)
+    pool.query('SELECT * from messages').then(res => {
+      socket.emit('chats', res.rows)
+    })
+  })
+  socket.on('movement', data => {
+    const player = players[socket.id] || {}
+    if (data.left) {
+      player.x -= 5
+    }
+    if (data.up) {
+      player.y -= 5
+    }
+    if (data.right) {
+      player.x += 5
+    }
+    if (data.down) {
+      player.y += 5
+    }
+  })
 
-app.get('/', function(request, response) {
-    response.sendFile(path.join(__dirname, 'static/index.html'));
-});
+  socket.on('startmove', () => {
+    const player = players[socket.id] || {}
+    player.moving = true
+  })
+  socket.on('endmove', () => {
+    const player = players[socket.id] || {}
+    player.moving = false
+  })
 
-
-server.listen(5000);
-console.log('Started on port 5000');
-
-
-var players = {};
-var catfood = {};
-var items = 0;
-
-//Add the WebSocket handlers
-io.on('connection', function(socket){
-    console.log('a user connected', socket.id);
-    socket.on('new player', (data) => {
-        let x = Math.floor(Math.random()*1000)+2000;
-        let y = Math.floor(Math.random()*800)+1000;
-        players[socket.id] = {
-            x: x,
-            y: y,
-            username: data
-        };
-        socket.emit('initstate', socket.id);
-        pool.query('SELECT * from messages').then((res) => {
-            socket.emit('chats', res.rows);
-        });
-    });
-    socket.on('movement', function(data) {
-        var player = players[socket.id] || {};
-        if (data.left) {
-            player.x -= 5;
+  socket.on('disconnect', () => {
+    delete players[socket.id]
+    console.log('user disconnected', socket.id)
+  })
+  socket.on('chat message', msg => {
+    const player = players[socket.id] || {}
+    pool
+      .query('INSERT INTO messages(msg, username) VALUES($1, $2)', [
+        msg,
+        player.username,
+      ])
+      .then(() => {
+        const obj = {
+          msg,
+          username: player.username,
+          created_at: new Date(),
         }
-        if (data.up) {
-            player.y -= 5;
-        }
-        if (data.right) {
-            player.x += 5;
-        }
-        if (data.down) {
-            player.y += 5;
-        }
-    });
+        io.emit('chat message', obj)
+      })
+  })
+})
 
-    socket.on('startmove', function() {
-        var player = players[socket.id] || {};
-        player.moving = true;
-    });
-    socket.on('endmove', function() {
-        var player = players[socket.id] || {};
-        player.moving = false;
-    });
+setInterval(() => {
+  io.sockets.emit('state', players)
+}, 1000 / 20)
 
-    socket.on('disconnect', function(){
-        delete players[socket.id];
-        console.log('user disconnected', socket.id);
-    });
-    socket.on('chat message', (msg) => {
-        var player = players[socket.id] || {};
-        pool.query('INSERT INTO messages(msg, username) VALUES($1, $2)', [msg, player.username]).then(() => {
-            var obj = {msg: msg, username: player.username, created_at: new Date()};
-            io.emit('chat message', obj);
-        });
-    });
-});
-
-
-setInterval(function() {
-    io.sockets.emit('state', players);
-}, 1000 / 20);
-
-setInterval(function() {
-    let x = Math.floor(Math.random()*1000)+2000;
-    let y = Math.floor(Math.random()*800)+1000;
-    catfood[items++] = {
-        x, y
-    };
-}, 10000);
-
-
+setInterval(() => {
+  const x = Math.floor(Math.random() * 1000) + 2000
+  const y = Math.floor(Math.random() * 800) + 1000
+  items += 1
+  catfood[items] = {
+    x,
+    y,
+  }
+}, 10000)
--- static/app.js
+++ static/app.js
@@ -1,227 +1,232 @@
-/* global io, $, moment */
+/* global io, $, moment, document, localStorage, prompt, Image */
 
-
-let can = document.getElementById('canvas1');
-var ctx = can.getContext('2d');
+const can = document.getElementById('canvas1')
+const ctx = can.getContext('2d')
 
 // player's position
-var playerid;
-var allPlayers = {};
+let playerid
+let allPlayers = {}
 
 // how far offset the canvas is
-let frame = 0;
-let counter = 0;
-let arrayWidth = 5;
-let arrayHeight = 5;
-let imageWidth = 2800;
-let imageHeight = 1600;
-let imageArray = [];
-let bird = [];
-let gratiot;
-let gameStarted = false;
-let timer;
-let username;
+let frame = 0
+let counter = 0
+const arrayWidth = 5
+const arrayHeight = 5
+const imageWidth = 2800
+const imageHeight = 1600
+let imageArray = []
+const bird = []
+let gratiot
+let gameStarted = false
+let timer
+let username
 
 if (localStorage.getItem('username') === null) {
-    username = prompt('Set a username');
+  username = prompt('Set a username')
 } else {
-    username = localStorage.getItem('username');
+  username = localStorage.getItem('username')
 }
-localStorage.setItem('username', username);
+localStorage.setItem('username', username)
 
-var socket = io(window.location.origin, { path: '/socket.io' });
+const socket = io(window.location.origin, { path: '/socket.io' })
 
 function draw() {
-    Object.keys(allPlayers).forEach((player) => {
-        if(allPlayers[player].moving) {
-            allPlayers[player].frame = Math.floor(((counter++) % 8) / 4);
-        }
-        ctx.drawImage(bird[allPlayers[player].frame||0], allPlayers[player].x, allPlayers[player].y, 100, 100);
-    });
-}
-
-//stackoverflow
-function pad(n, width, z) {
-    z = z || '0';
-    n = n + '';
-    return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
-}
-
-
-
-function setup(){
-    let imagesLoading = [];
-    let images = [];
-    for(let i = 0; i < 25; i++) {
-        images[i] = `static/tiles/tile${pad(i,3)}.png`;
+  Object.keys(allPlayers).forEach(player => {
+    if (allPlayers[player].moving) {
+      allPlayers[player].frame = Math.floor((counter % 8) / 4)
+      counter += 1
     }
-    images.push('static/img/bird0.png');
-    images.push('static/img/bird1.png');
-    images.push('static/img/gratiot.png');
-
-    for(var i = 0; i < images.length; i++) {
-        var imageObj = new Image();
-        imagesLoading[i] = new Promise((resolve) => {
-            imageObj.onload = function() {
-                resolve(this);
-            };
-            imageObj.src = images[i];
-        });
-    }
-
-    console.log('images loading');
-    Promise.all(imagesLoading).then((imageArray) => {
-        bird[0] = imageArray[25];
-        bird[1] = imageArray[26];
-        gratiot = imageArray[27];
-
-        console.log('done loading');
-        window.requestAnimationFrame(myRenderTileSetup);
-    });
+    ctx.drawImage(
+      bird[allPlayers[player].frame || 0],
+      allPlayers[player].x,
+      allPlayers[player].y,
+      100,
+      100,
+    )
+  })
 }
 
+// stackoverflow
+function pad(input, width, z = '0') {
+  const n = `${input}`
+  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n
+}
 
 function myRenderTileSetup() {
-    if(gameStarted) {
-        console.log('here');
-        clearInterval(timer);
-        ctx.save();
-        let offsetX = allPlayers[playerid].x;
-        let offsetY = allPlayers[playerid].y;
-        ctx.translate(-offsetX+250, -offsetY+250);
-        ctx.clearRect(0, 0, can.width, can.height);
-        for(let y = 0; y < arrayWidth; y++){
-            for(let x = 0; x < arrayHeight; x++){
-                let pos = x + y * arrayWidth;
-                if(imageArray[pos] && imageArray[pos].complete){
-                    ctx.drawImage(imageArray[pos],(pos%arrayWidth)*imageWidth, Math.floor(pos/arrayWidth)*imageHeight);
-                }
-            }
+  if (gameStarted) {
+    clearInterval(timer)
+    ctx.save()
+    const offsetX = allPlayers[playerid].x
+    const offsetY = allPlayers[playerid].y
+    ctx.translate(-offsetX + 250, -offsetY + 250)
+    ctx.clearRect(0, 0, can.width, can.height)
+    for (let y = 0; y < arrayWidth; y += 1) {
+      for (let x = 0; x < arrayHeight; x += 1) {
+        const pos = x + y * arrayWidth
+        if (imageArray[pos] && imageArray[pos].complete) {
+          ctx.drawImage(
+            imageArray[pos],
+            (pos % arrayWidth) * imageWidth,
+            Math.floor(pos / arrayWidth) * imageHeight,
+          )
         }
-        draw();
-
-        ctx.restore();
-    } else {
-        ctx.save();
-        ctx.drawImage(gratiot, 0, 0, 800, 600);
-        if(!timer) {
-            timer = setInterval(function() {
-                frame = (frame+1)%2;
-            }, 400);
-        }
-        ctx.restore();
+      }
     }
+    draw()
 
-    window.requestAnimationFrame(myRenderTileSetup);
+    ctx.restore()
+  } else {
+    ctx.save()
+    ctx.drawImage(gratiot, 0, 0, 800, 600)
+    if (!timer) {
+      timer = setInterval(() => {
+        frame = (frame + 1) % 2
+      }, 400)
+    }
+    ctx.restore()
+  }
+
+  window.requestAnimationFrame(myRenderTileSetup)
 }
 
-var movement = {
-    up: false,
-    down: false,
-    left: false,
-    right: false
-};
-document.addEventListener('keydown', function(event) {
-    switch (event.keyCode) {
-    case 37:
-        movement.left = true;
-        break;
-    case 38:
-        movement.up = true;
-        break;
-    case 39:
-        movement.right = true;
-        break;
-    case 40:
-        movement.down = true;
-        break;
-    case 32:
-        gameStarted = true;
-        break;
-    }
-    if(movement.left||movement.up||movement.right||movement.down) {
-        socket.emit('startmove');
-    }
-});
-document.addEventListener('keyup', function(event) {
-    switch (event.keyCode) {
-    case 37:
-        movement.left = false;
-        break;
-    case 38:
-        movement.up = false;
-        break;
-    case 39:
-        movement.right = false;
-        break;
-    case 40:
-        movement.down = false;
-        break;
-    }
-    if(!movement.left&&!movement.up&&!movement.right&&!movement.down) {
-        socket.emit('endmove');
-    }
-});
+function setup() {
+  const imagesLoading = []
+  const images = []
+  for (let i = 0; i < 25; i += 1) {
+    images[i] = `static/tiles/tile${pad(i, 3)}.png`
+  }
+  images.push('static/img/bird0.png')
+  images.push('static/img/bird1.png')
+  images.push('static/img/gratiot.png')
 
-can.addEventListener('touchstart', touchHandler);
-can.addEventListener('touchmove', touchHandler);
-can.addEventListener('touchend', function() {
-    movement.right = false;
-    movement.left = false;
-    movement.up = false;
-    movement.down = false;
-    socket.emit('endmove');
-});
+  for (let i = 0; i < images.length; i += 1) {
+    const imageObj = new Image()
+    imagesLoading[i] = new Promise(resolve => {
+      imageObj.onload = function() {
+        resolve(this)
+      }
+      imageObj.src = images[i]
+    })
+  }
+
+  Promise.all(imagesLoading).then(res => {
+    imageArray = res
+    bird[0] = imageArray[25]
+    bird[1] = imageArray[26]
+    gratiot = imageArray[27]
+
+    window.requestAnimationFrame(myRenderTileSetup)
+  })
+}
+
+const movement = {
+  up: false,
+  down: false,
+  left: false,
+  right: false,
+}
+document.addEventListener('keydown', event => {
+  switch (event.keyCode) {
+    case 37:
+      movement.left = true
+      break
+    case 38:
+      movement.up = true
+      break
+    case 39:
+      movement.right = true
+      break
+    case 40:
+      movement.down = true
+      break
+    case 32:
+      gameStarted = true
+      break
+    default:
+  }
+  if (movement.left || movement.up || movement.right || movement.down) {
+    socket.emit('startmove')
+  }
+})
+document.addEventListener('keyup', event => {
+  switch (event.keyCode) {
+    case 37:
+      movement.left = false
+      break
+    case 38:
+      movement.up = false
+      break
+    case 39:
+      movement.right = false
+      break
+    case 40:
+      movement.down = false
+      break
+    default:
+  }
+  if (!movement.left && !movement.up && !movement.right && !movement.down) {
+    socket.emit('endmove')
+  }
+})
 
 function touchHandler(e) {
-    if(!gameStarted) {
-        gameStarted = true;
-        return;
+  if (!gameStarted) {
+    gameStarted = true
+    return
+  }
+  if (e.touches) {
+    const playerX = e.touches[0].pageX - can.offsetLeft
+    const playerY = e.touches[0].pageY - can.offsetTop
+    if (playerX > 420) movement.right = true
+    if (playerX < 320) movement.left = true
+    if (playerY > 340) movement.down = true
+    if (playerY < 260) movement.up = true
+    if (movement.left || movement.up || movement.right || movement.down) {
+      socket.emit('startmove')
     }
-    if(e.touches) {
-        const playerX = e.touches[0].pageX - can.offsetLeft;
-        const playerY = e.touches[0].pageY - can.offsetTop;
-        if(playerX > 420) movement.right = true;
-        if(playerX < 320) movement.left = true;
-        if(playerY > 340) movement.down = true;
-        if(playerY < 260) movement.up = true;
-        if(movement.left||movement.up||movement.right||movement.down) {
-            socket.emit('startmove');
-        }
-    }
+  }
 }
+can.addEventListener('touchstart', touchHandler)
+can.addEventListener('touchmove', touchHandler)
+can.addEventListener('touchend', () => {
+  movement.right = false
+  movement.left = false
+  movement.up = false
+  movement.down = false
+  socket.emit('endmove')
+})
 
+setup()
 
-setup();
+socket.emit('new player', username)
 
-socket.emit('new player', username);
+setInterval(() => {
+  socket.emit('movement', movement)
+}, 1000 / 60)
 
-setInterval(function() {
-    socket.emit('movement', movement);
-}, 1000 / 60);
+socket.on('initstate', data => {
+  playerid = data
+})
 
-socket.on('initstate', function(data) {
-    playerid = data;
-});
+socket.on('chats', data => {
+  data.forEach(msg => {
+    const ts = moment(msg.created_at).format('MMMM Do YYYY, h:mm:ss a')
+    $('#messages').prepend(
+      $('<li>').text(`(${ts}) ${msg.username}: ${msg.msg}`),
+    )
+  })
+})
 
-socket.on('chats', function(data) {
-    data.forEach((msg) => {
-        var ts = moment(msg.created_at).format('MMMM Do YYYY, h:mm:ss a');
-        $('#messages').prepend($('<li>').text('('+ts+') ' + msg.username + ': ' + msg.msg));
-    });
-});
+socket.on('chat message', msg => {
+  const ts = moment(msg.created_at).format('MMMM Do YYYY, h:mm:ss a')
+  $('#messages').prepend($('<li>').text(`(${ts}) ${msg.username}: ${msg.msg}`))
+})
+$('form').submit(() => {
+  socket.emit('chat message', $('#m').val())
+  $('#m').val('')
+  return false
+})
 
-socket.on('chat message', function(msg){
-    var ts = moment(msg.created_at).format('MMMM Do YYYY, h:mm:ss a');
-    $('#messages').prepend($('<li>').text('('+ts+') ' + msg.username + ': ' + msg.msg));
-});
-$('form').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
-    return false;
-});
-
-socket.on('state', function(players) {
-    allPlayers = players;
-});
-
+socket.on('state', players => {
+  allPlayers = players
+})
